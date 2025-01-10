04718a5bfa8f18972e976185583e201ca8aed166
gianinna <gi.causa@gmail.com>
2025-01-10 00:41:13 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helppet',
+  webDir: 'www'
+};
+
+export default config;
